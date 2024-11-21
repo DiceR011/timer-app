5d88231a6bdec7f6bd0bdea36a2484083ec5dd5c
DiceR011 <touhu0305@gmail.com>
2024-11-21 13:08:53 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Timer from "./components/Timer";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ポモドーロタイマー</h1>
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
